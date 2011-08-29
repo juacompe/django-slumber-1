b51999f689721984fe0f8a9284bbb5a6c73362c2
Kirit Sælensminde <k@kirit.com>
2011-08-29 21:06:51 +0700

    Started to implement a data stack.

--- slumber/test.py
+++ slumber/test.py
@@ -18,11 +18,11 @@ def _do_get(model, **query):
     assert False, "The instance was not found"
 
 
-class _MockClient(DictObject):
-    """Mock slumber client class.
+class _DataConfiguration(DictObject):
+    """Mock slumber data.
     """
     def __init__(self, **instances):
-        super(_MockClient, self).__init__()
+        super(_DataConfiguration, self).__init__()
         for model, instances in instances.items():
             root = self
             for k in model.split('__')[:-1]:
@@ -36,24 +36,38 @@ class _MockClient(DictObject):
             setattr(model_type, 'get', classmethod(_do_get))
             setattr(root, model_name, model_type)
 
+
+class _MockClient(object):
+    """A mock client that searches the current data stack for the data.
+    """
+    DATA_STACK = []
     def _flush_client_instance_cache(self):
         """Empty stub so that the middleware works in tests.
         """
+
+    def __getattr__(self, name):
+        """Try to find the attribute in the data stack.
+        """
+        return getattr(_MockClient.DATA_STACK[-1], name)
 
 
 def mock_client(**instances):
     """Replaces the client with a mocked client that provides access to the
     provided applications, models and instances.
     """
-    models = _MockClient(**instances)
+    data = _DataConfiguration(**instances)
     def decorator(test_method):
         """The actual decorator that is going to be used on the test method.
         """
-        @mock.patch('slumber._client', models)
+        @mock.patch('slumber._client', _MockClient())
         def test_wrapped(test, *a, **kw):
             """The wrapper for the test method.
             """
-            test_method(test, *a, **kw)
+            try:
+                _MockClient.DATA_STACK.insert(0, data)
+                return test_method(test, *a, **kw)
+            finally:
+                _MockClient.DATA_STACK.remove(data)
         test_wrapped.__doc__ = test_method.__doc__
         return test_wrapped
     return decorator
